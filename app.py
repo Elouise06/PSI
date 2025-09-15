08b41bf490fd38b21955b871654f5d0c063a7f41
Elouise06 <160539137+Elouise06@users.noreply.github.com>
2025-09-15 17:22:47 +0000

    atualizando e encerrando

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return 'Olá Mundo'
+  return 'Essa é minha primeira aplicação em Flask!'
+
+@app.route('/contato')
+def contato():
+  return 'elouise.costa@escolar.ifrn.edu.br'
+
+if __name__ == '__main__':
+    app.run()
