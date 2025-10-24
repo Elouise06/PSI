6e57a9d558e358eeeb27484a7448b899bc5a988b
Elouise06 <160539137+Elouise06@users.noreply.github.com>
2025-10-24 20:40:26 +0000

    aula python 24/10/2025

---
+++ Aulas-thiago/main.py
@@ -0,0 +1,49 @@
+# 01 - Crie duas variaveis em que A = 10 e B = 20,
+# em seguida faça A = B e B = A
+# de modo a obter os resultados finais A = 20 e B = 10.
+
+# usuario_1 = "josé Caetano da Silva"
+# usuario_2 = "Maria Souza Lemos"
+
+# print("usuario 1: ", usuario_1)
+# print("usuario 2: ", usuario_2)
+
+#troca_usuario = usuario_1
+#usuario_1 = usuario_2
+#usuario_2 = troca_usuario
+
+#print(usuario_1)
+#print(usuario_2)
+
+
+# 03 - Crie uma função que recebe um valor e retorna a 
+# informação dizendo se é um valor positivo, negativo ou nulo.
+
+#ef avalia_valor(numero):
+#  if type(numero) == int:
+#       if numero > 0:
+#           return 'O número é positivo'
+#       elif numero < 0:
+#           return 'O número é negativo'
+#       elif numero == 0:
+#           return 'O número é nulo'
+#   else:
+#       return 'Valor inválido'
+    
+#rint(avalia_valor("lolo"))
+
+
+#O custo de um carro novo ao consumidor é a soma do custo de
+#fábrica com a porcetagem do distribuidor e dos impostos
+#(aplicados ao custo de fábrica). Supondo que o percentual do
+#distribuidor seja de 28% e os impostos de 45%, escrever um
+#algoritmo para ler o custo de fábrica de um carro,
+#calcular e escrever o custo final ao consumior.
+
+# def calular_custo(custo_fabrica):
+#   imposto_distribuidor = custo_fabrica*0.28
+#   imposto_geral = custo_fabrica*0.45
+#  custo_total = custo_fabrica + imposto_distribuidor + imposto_geral
+#   return custo_total
+
+#print(calcular_custo((10000))
